9c96733a02a07118a4e578176281716d0461e4e8
Ajopratama1 <a.ajopratama01@gmail.com>
2025-11-06 14:50:31 +0700

    Rapiin struktur halaman FormIsian (pindah anotasi & pasang Top Bar

---
+++ app/src/main/java/com/example/questnavigasiui_057/view/Formulir.kt
@@ -0,0 +1,52 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package com.example.questnavigasiui_057.view
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.OutlinedTextField // Diimpor yang benar
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.questnavigasiui_057.R
+
+
+@OptIn(ExperimentalMaterial3Api::class) // Anotasi dipindahkan ke fungsi yang relevan
+@Composable
+fun FormIsian(
+    jenisK: List<String> = listOf("Laki-laki", "Perempuan"),
+    OnSubmitBtnClick: () -> Unit
+) {
+    Scaffold(
+        topBar = { // 1. TopAppBar harus berada di dalam parameter topBar
+            TopAppBar(
+                title = {
+                    Text(
+                        stringResource(id = R.string.home),
+                        color = Color.White
+                    )
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    // 2. Tentukan nama parameter 'containerColor'
+                    containerColor = colorResource(id = R.color.teal_700)
+                )
+            )
+        }
