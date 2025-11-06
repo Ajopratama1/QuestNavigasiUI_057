f86925cf505a833b609c3ade040b18be98ca0311
Ajopratama1 <a.ajopratama01@gmail.com>
2025-11-06 14:25:11 +0700

    memanggil fungsi

--- app/src/main/java/com/example/questnavigasiui_057/MainActivity.kt
+++ app/src/main/java/com/example/questnavigasiui_057/MainActivity.kt
@@ -20,28 +20,12 @@ class MainActivity : ComponentActivity() {
         setContent {
             QuestNavigasiUI_057Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    DataApp(
+
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    QuestNavigasiUI_057Theme {
-        Greeting("Android")
     }
 }
