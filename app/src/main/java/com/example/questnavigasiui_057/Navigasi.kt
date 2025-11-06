b0e53bfe88e48de8ded7333447c65047e1002c73
Ajopratama1 <a.ajopratama01@gmail.com>
2025-11-06 12:54:59 +0700

    Definisikan rute navigasi (Formulir dan Detail)

--- app/src/main/java/com/example/questnavigasiui_057/Navigasi.kt
+++ app/src/main/java/com/example/questnavigasiui_057/Navigasi.kt
@@ -1,4 +1,18 @@
 package com.example.questnavigasiui_057
 
-class Navigasi {
+import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+
+enum class Navigasi {
+    Formulirku,
+    Detail
 }
+
+
